fb38b935f54431aba5c4641ed82e30fe2be59cc2
David Valencia <davidvalencia0526@gmail.com>
2023-10-29 14:51:21 -0500

    arregle manejo de mensajes de recuperar contrasena

--- src/components/pages/Login/Form.jsx
+++ src/components/pages/Login/Form.jsx
@@ -74,8 +74,12 @@ export default function Form() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          {error && <p className="text-red-600 mb-4">{error}</p>}
-          <div className="mt-8 flex flex-col gap-y-2">
+          {error && (
+              <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative mt-4 text-center">
+                {error}
+              </div>
+            )}
+          <div className="mt-4 flex flex-col gap-y-2">
             <Link to="/cacharreria_cosas_bonitas/CambioContrasena/">
             
             <button className=" font-medium pl-4 text-base text-romTurquoise-600">
