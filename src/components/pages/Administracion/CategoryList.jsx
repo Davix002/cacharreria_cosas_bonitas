cd7a091960409ad11ed2dc474b18babf4d14864a
David Valencia <davidvalencia0526@gmail.com>
2023-10-30 02:45:54 -0500

    edicion en crud categoria con ventana modal

--- src/components/pages/Administracion/CategoryList.jsx
+++ src/components/pages/Administracion/CategoryList.jsx
@@ -4,7 +4,7 @@ import {
   getCategories,
   deleteCategory,
   createCategory,
-  uploadCategoryImage
+  uploadCategoryImage,
 } from "../../../config/api/apiUtils";
 
 const CategoryList = () => {
@@ -12,6 +12,7 @@ const CategoryList = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [categoryImage, setCategoryImage] = useState(null);
   const [newCategoryName, setNewCategoryName] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -30,38 +31,38 @@ const CategoryList = () => {
 
   const handleFormSubmit = (updatedCategory) => {
     if (updatedCategory && updatedCategory._id) {
-        const updatedCategories = categories.map(cat => 
-            cat._id === updatedCategory._id ? updatedCategory : cat
-        );
-        setCategories(updatedCategories);
+      const updatedCategories = categories.map((cat) =>
+        cat._id === updatedCategory._id ? updatedCategory : cat
+      );
+      setCategories(updatedCategories);
     }
     setSelectedCategory(null);
-};
+  };
 
   const handleAddCategory = async () => {
     if (newCategoryName.trim() === "") {
       alert("El nombre de la categoría no puede estar vacío.");
       return;
     }
-  
+
     if (!categoryImage) {
       alert("Por favor, selecciona una imagen para la categoría.");
       return;
     }
-  
+
     try {
       // Primero sube la imagen
       const imageResponse = await uploadCategoryImage(categoryImage);
-      
+
       // Luego crea la categoría con el nombre y la URL de la imagen
       const newCategory = await createCategory({
         name: newCategoryName,
-        picture: imageResponse.imageUrl
+        picture: imageResponse.imageUrl,
       });
-  
+
       // Actualiza el estado local con la nueva categoría
       setCategories([...categories, newCategory]);
-  
+
       // Limpia los inputs
       setNewCategoryName("");
       setCategoryImage(null);
@@ -69,7 +70,6 @@ const CategoryList = () => {
       console.error("Error al agregar la categoría:", error);
     }
   };
-  
 
   const handleImageChange = (e) => {
     setCategoryImage(e.target.files[0]);
@@ -77,12 +77,7 @@ const CategoryList = () => {
 
   return (
     <div className="bg-gray-100 p-6 rounded-md place-items-center shadow-md w-full flex flex-col justify-center items-center">
-      {selectedCategory ? (
-        <CategoryForm
-          categoryToUpdate={selectedCategory}
-          onFormSubmit={handleFormSubmit}
-        />
-      ) : (
+      {
         <>
           <div className="w-2/4 mb-4">
             <input
@@ -113,7 +108,10 @@ const CategoryList = () => {
                   <td className="border-t py-2 px-4">{cat.name}</td>
                   <td className="border-t py-2 px-4 just flex justify-around">
                     <button
-                      onClick={() => setSelectedCategory(cat)}
+                      onClick={() => {
+                        setSelectedCategory(cat);
+                        setIsModalOpen(true);
+                      }}
                       className="mr-2 rounded-md shadow-md text-romTurquoise-600 hover:text-blue-800 focus:outline-none"
                     >
                       Edit
@@ -129,8 +127,28 @@ const CategoryList = () => {
               ))}
             </tbody>
           </table>
+
+          {isModalOpen && (
+            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-black bg-opacity-50">
+              <div className="bg-white p-4 rounded-md shadow-lg relative w-2/4">
+                <button
+                  onClick={() => setIsModalOpen(false)}
+                  className="absolute top-2 right-2"
+                >
+                  X
+                </button>
+                <CategoryForm
+                  categoryToUpdate={selectedCategory}
+                  onFormSubmit={(updatedCategory) => {
+                    handleFormSubmit(updatedCategory);
+                    setIsModalOpen(false);
+                  }}
+                />
+              </div>
+            </div>
+          )}
         </>
-      )}
+      }
     </div>
   );
 };
